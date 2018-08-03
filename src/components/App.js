a8c0e8c6744cbb86c5d4eaf478d3f5e6b0582cd8
jackdbd <jackdebidda@gmail.com>
2018-08-03 12:58:09 +0200

    Add react spinners (to show when loading data)

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { PropagateLoader } from "react-spinners";
+import { Scrollama } from "./Scrollama";
 import { Intro, ScreenA, ScreenB, Outro } from "./Screens";
 
 class App extends Component {
@@ -8,10 +10,31 @@ class App extends Component {
 
   render() {
     const { stories } = this.state;
+    // we need a scrollama instance for each story (i.e. a scrollable section)
+    const scrollamaOptionsList = stories.map((d, i) => {
+      const opt = {
+        container: `scroll__container_${i}`,
+        graphic: `scroll__graphic_${i}`,
+        text: `scroll__text_${i}`,
+        step: `scroll__step_${i}`,
+        offset: 0.5 + (i + 1) * 0.1,
+        progress: true,
+        debug: true
+      };
+      return opt;
+    });
     return (
       <div className="App">
         <Intro fontFamily={"Lobster"} />
-        <div>TODO: scrollama component here</div>
+        {stories.length > 0 ? (
+          <div>TODO: scrollama component here</div>
+        ) : (
+          <PropagateLoader
+            color={"#0336ff"}
+            loading={true}
+            loaderStyle={{ left: "50%", top: "40%" }}
+          />
+        )}
         <ScreenA fontFamily={"Kalam"} backgroundColor={"red"} />
         <div>TODO: scrollama component here</div>
         <ScreenB backgroundColor={"blue"} />
